4864cd36e114dae8e4d9f74f3d2491cd1a17bf63
= <=>
2016-02-16 19:35:27 +0100

    Edited the message on scope in login directive

--- src/webapp/js/login/loginDirective.js
+++ src/webapp/js/login/loginDirective.js
@@ -17,6 +17,18 @@
                                 $location.path("/test");
                             })
                             .catch(function (response) {
+                                switch(response.status){
+                                    case 401:
+                                        scope.message = "Invalid credentials!";
+                                        break;
+                                    case -1:
+                                        scope.message = "There is something wrong with the connection!";
+                                        break;
+                                    default:
+                                        scope.message = "There is something wrong please contact a administrator!";
+                                        break;
+                                }
+                                // console.log(response);
                                 // TODO: HANDLE ERROR console.log(response.status);
                             });
                         }
